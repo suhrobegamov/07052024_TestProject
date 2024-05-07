8116b3d90e0eba10f59b423854f4e94ddfbcdae3
Suhrob Egamov <75565717+suhrobegamov@users.noreply.github.com>
2024-05-07 16:22:23 +0300

    Добавьте файлы проекта.

---
+++ 07052024_TestProject/Program.cs
@@ -0,0 +1,16 @@
+﻿long nomenclatureId = 148748593;
+string url = $"https://www.wildberries.ru/catalog/{nomenclatureId}/detail.aspx";
+
+try
+{
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        Console.WriteLine((await response.Content.ReadAsStringAsync()));
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
